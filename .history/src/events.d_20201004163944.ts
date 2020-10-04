07bd0015d196cad6e8387f9ef235880ea1f8a060
Cave_Johnson <andreas.rossa@subseq.net>
2020-10-04 17:33:16 +0200

    remove history

--- .history/src/events.d_20201004163944.ts
+++
@@ -1,5 +0,0 @@
-import { Pos2D } from "./warehouse_lib";
-
-export interface RobotMoveEvent {
-  moveTo: Pos2D
-}
--- .history/src/robot_client_20201004155450.ts
+++
@@ -1,4 +0,0 @@
-import robot = require("robot")
-import component = require("component")
-import warehouse = require("./warehouse_lib")
-import controller = require("./controller")
