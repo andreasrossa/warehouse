9561cbdddffd2286e1ea208d056013d1f8395370
Andreas Rossa <andreas@rossamail.de>
2020-10-04 18:02:10 +0200

    Add lualib_bundle to init

--- dist/init.lua
+++ dist/init.lua
@@ -4,3 +4,4 @@ sh.execute("wget -fw https://raw.githubusercontent.com/andreasrossa/warehouse/ma
 sh.execute("wget -fw https://raw.githubusercontent.com/andreasrossa/warehouse/master/dist/controller.lua")
 sh.execute("wget -fw https://raw.githubusercontent.com/andreasrossa/warehouse/master/dist/find_waypoints.lua")
 sh.execute("wget -fw https://raw.githubusercontent.com/andreasrossa/warehouse/master/dist/robot_client.lua")
+sh.execute("wget -fw https://raw.githubusercontent.com/andreasrossa/warehouse/master/dist/lualib_bundle.lua")
